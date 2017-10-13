6ac9fd74a3bdd3998e5286dde4b9b43f9084f493
Unknown <h6zhan@gmail.com>
2017-10-13 19:03:40 -0400

    fixed issue with get_random_var and compute_performance

--- lab1/question2.py
+++ lab1/question2.py
@@ -1,22 +1,18 @@
 import queue_simulation
-import math
-import random
+import time
 
 TICKS = 1000000
 L = 2000
 C = 1000000
 
-print("p,E[N],E[T],P_IDLE")
+f = open("result/q2_{}.csv".format(int((time.time()-1507925570)/30)), 'w')
+f.write("p,E[N],E[T],P_IDLE")
 
 for i in range(2, 10):
     p = i / 10
     LAM = p * C / L
+    print('simulating p =', p)
     EN, ET, P_LOSS, P_IDLE = queue_simulation.main([TICKS, LAM, L, C])
-    print("{},{},{},{}".format(p, EN, ET, P_IDLE))
-    # print(LAM)
-    # random_var = 1000000 * (-1 / LAM) * math.log(1 - random.random())
-    # print(random_var)
-# en = list()
-# en.append(1)
-# en.append(1.1)
-# print(sum(en))
+    f.write("\n{},{},{},{}".format(p, EN, ET, P_IDLE))
+
+f.close()
