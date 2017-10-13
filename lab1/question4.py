8722f6d075c8126d9dbb99f8b379e4dca1d9c1ff
Henry Zhu <hrzhu@uwaterloo.ca>
2017-10-12 22:15:24 -0400

    Merge branch 'master' of https://github.com/philip-zhan/ece358
    add q4

--- lab1/question4.py
+++ lab1/question4.py
@@ -1,9 +1,21 @@
 import queue_simulation
 
 TICKS = 100000
-L = 20000
+L = 2000
 C = 1000000
-for i in range(2, 10):
-    mu = i / 10
-    LAM = mu * C / L
-    queue_simulation.main([TICKS, LAM, L, C])
+K1 = 10
+K2 = 25
+K3 = 50
+for i in range(5, 15):
+	mu = i / 10
+	LAM = mu * C / L
+	queue_simulation.main([TICKS, LAM, L, C, K1])
+for i in range(5, 15):
+	mu = i / 10
+	LAM = mu * C / L
+	queue_simulation.main([TICKS, LAM, L, C, K2])
+for i in range(5, 15):
+	mu = i / 10
+	LAM = mu * C / L
+	queue_simulation.main([TICKS, LAM, L, C, K3])
+
